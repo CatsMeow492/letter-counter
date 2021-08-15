1b51befc4619a7be6965dd55471ea5679ebeb54e
CatsMeow492 <56234110+CatsMeow492@users.noreply.github.com>
2021-08-14 17:48:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ dict = {}
 
 alphabet = list(string.ascii_lowercase)
 
+
+for letter in text:
+    if alphabet.count(letter) != 0:
+        dict[letter] = text.count(letter)
+        
+print("In order of appearence: ")
+print(dict)
+
+dict = {}
+#your code goes here
+
+alphabet = list(string.ascii_lowercase)
+
 for i in range(26):
     letter = alphabet[i] 
     if text.count(letter) != 0:
         dict[letter] = text.count(letter)
         
-
+print("In alphabetical order: ")
 print(dict)
